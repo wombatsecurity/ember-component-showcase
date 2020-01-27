c0b2d727aacdc3e8d733c6300120cd53bbb7e643
nmcclay <nmcclay@proofpoint.com>
2020-01-27 13:23:04 -0500

    test tweaks

--- tests/integration/components/component-showcase-test.js
+++ tests/integration/components/component-showcase-test.js
@@ -18,46 +18,46 @@ module('Integration | Component | component showcase', function(hooks) {
     {{s.source hbs=''}}
   {{/component-showcase}}`);
 
-    assert.dom('.card-title').hasText('','renders no title when empty');
-    assert.dom('.description').hasText('', 'renders no description when empty');
-    assert.dom('.tab-nav > *').exists({count: 1});
-    assert.equal(find('.tab-nav').textContent.trim(), 'example.hbs', 'only tab visible is Handlebars');
+    assert.dom('.showcase-title').hasText('','renders no title when empty');
+    assert.dom('.showcase-body .description').hasText('', 'renders no body when empty');
+    assert.dom('.showcase-tabs > *').exists({count: 1});
+    assert.equal(find('.showcase-tabs').textContent.trim(), 'example.hbs', 'only tab visible is Handlebars');
 
     await render(hbs`
   {{#component-showcase "123" "Title" as |s|}}
-    {{s.docs "description"}}
+    {{s.docs "body"}}
     {{#s.example}}
       {{showcase/s-icon "code"}}
     {{/s.example}}
     {{s.source hbs='{{fa-icon "code"}}'}}
   {{/component-showcase}}`);
 
-    assert.dom('.card-title').hasText('Title', 'renders custom title');
-    assert.dom('.description').hasText('description', 'renders custom description');
+    assert.dom('.showcase-title').hasText('Title', 'renders custom title');
+    assert.dom('.showcase-body .description').hasText('body', 'renders custom body');
 
     await render(hbs`
   {{#component-showcase "123" as |s|}}
-    {{s.docs "description"}}
+    {{s.docs "body"}}
     {{#s.example}}
       {{showcase/s-icon "code"}}
     {{/s.example}}
     {{s.source showHTML=true}}
   {{/component-showcase}}`);
 
-    assert.dom('.tab-nav > *').exists({count: 2});
-    assert.dom('.tab-nav').hasText('example.hbs markup.html');
+    assert.dom('.showcase-tabs > *').exists({count: 2});
+    assert.dom('.showcase-tabs').hasText('example.hbs markup.html');
 
     await render(hbs`
   {{#component-showcase "123" as |s|}}
-    {{s.docs "description"}}
+    {{s.docs "body"}}
     {{#s.example}}
       {{showcase/s-icon "code"}}
     {{/s.example}}
     {{s.source hideHBS=true showHTML=true}}
   {{/component-showcase}}`);
 
-    assert.dom('.tab-nav > *').exists({count: 1}, 'hideHBS hides hbs, and showHTML leaves 1 tab');
-    assert.dom('.tab-nav').hasText('markup.html', 'only tab visible is Markup');
+    assert.dom('.showcase-tabs > *').exists({count: 1}, 'hideHBS hides hbs, and showHTML leaves 1 tab');
+    assert.dom('.showcase-tabs').hasText('markup.html', 'only tab visible is Markup');
 
     await render(hbs`
   {{#component-showcase "123" as |s|}}
@@ -66,8 +66,8 @@ module('Integration | Component | component showcase', function(hooks) {
     {{/s.example}}
   {{/component-showcase}}`);
 
-    assert.dom('.tab-nav > *').doesNotExist('with no source, there are no tabs');
-    assert.dom('.description').doesNotExist('with no docs, there are no description');
+    assert.dom('.showcase-tabs > *').doesNotExist('with no source, there are no tabs');
+    assert.dom('.showcase-body .description').doesNotExist('with no docs, there are no body');
 
     await render(hbs`
   {{#component-showcase as |s|}}
@@ -77,8 +77,8 @@ module('Integration | Component | component showcase', function(hooks) {
     {{s.source (array (hash title="route.js" language="JavaScript" src="var foo = 'foo';")) showHTML=true }}
   {{/component-showcase}}`);
 
-    assert.dom('.tab-nav > *').exists({count: 3}, 'using showcase/s-source you can add custom tabs');
-    assert.dom('.tab-nav').hasText('example.hbs markup.html route.js', 'will show custom tab names and orders correctly');
+    assert.dom('.showcase-tabs > *').exists({count: 3}, 'using showcase/s-source you can add custom tabs');
+    assert.dom('.showcase-tabs').hasText('example.hbs markup.html route.js', 'will show custom tab names and orders correctly');
 
     await render(hbs`
   {{#component-showcase as |s|}}
@@ -91,7 +91,7 @@ module('Integration | Component | component showcase', function(hooks) {
     ) hideHBS=true }}
   {{/component-showcase}}`);
 
-    assert.dom('.tab-nav > *').exists({count: 2}, 'using showcase/s-source you can remove default tabs');
-    assert.dom('.tab-nav').hasText('Route.js Controller.js', 'will show custom tab names and orders correctly without defaults');
+    assert.dom('.showcase-tabs > *').exists({count: 2}, 'using showcase/s-source you can remove default tabs');
+    assert.dom('.showcase-tabs').hasText('Route.js Controller.js', 'will show custom tab names and orders correctly without defaults');
   });
 });
