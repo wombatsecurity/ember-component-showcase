32a48b8654700f3942f6f6f6abd35782649bb1f1
Brian Franco <mastastealth+git@gmail.com>
2020-09-04 14:28:55 -0400

    Clear out console

--- addon/components/showcase/s-docs.js
+++ addon/components/showcase/s-docs.js
@@ -1,22 +1,24 @@
-import Component from '@glimmer/component';
-import { inject as service } from '@ember/service';
+import Component from "@glimmer/component";
+import { inject as service } from "@ember/service";
 
-export default class SampleDocComponent extends  Component {
+export default class SampleDocComponent extends Component {
   @service documentation;
 
-  exampleLanguageRegex = new RegExp('^{(.*)}\\n(\\s*[.|\\n|\\W|\\w]*)');
+  exampleLanguageRegex = new RegExp("^{(.*)}\\n(\\s*[.|\\n|\\W|\\w]*)");
 
   parseExample(exampleSet) {
     let examples = [];
 
     for (let exampleContent of exampleSet) {
       let example = {
-        language: 'JavaScript',
+        language: "JavaScript",
         content: exampleContent.description,
-        title: 'Show Example'
+        title: "Show Example",
       };
 
-      let regexMatch = exampleContent.description.trim().match(this.exampleLanguageRegex);
+      let regexMatch = exampleContent.description
+        .trim()
+        .match(this.exampleLanguageRegex);
       if (regexMatch && regexMatch[1] && regexMatch[2]) {
         example.language = regexMatch[1];
         example.content = regexMatch[2];
@@ -27,21 +29,21 @@ export default class SampleDocComponent extends  Component {
     return examples.length > 0 ? examples : null;
   }
 
-	get classDocs() {
+  get classDocs() {
     let className = this.args.api;
     const docs = this.documentation.getClass(className);
 
     if (docs.name && !docs.modified) {
-      console.log('ACTUAL DOC', docs);
-
       // Fix descriptions
       docs.description = docs.description.children[0].children[0].value;
 
       // Fix/Add some lines per prop
-      docs.members.instance.forEach(inst => {
+      docs.members.instance.forEach((inst) => {
         inst.description = inst.description.children[0].children[0].value;
         inst.github = docs.github;
-        inst.file = inst.context.file.slice(inst.context.file.indexOf('addon/'));
+        inst.file = inst.context.file.slice(
+          inst.context.file.indexOf("addon/")
+        );
         inst.line = inst.context.loc.start.line;
         if (inst.examples) inst.examples = this.parseExample(inst.examples);
       });
@@ -49,11 +51,11 @@ export default class SampleDocComponent extends  Component {
       // Done modifying
       docs.modified = true;
     }
-  
+
     return docs;
   }
-  
-	get apiDocs() {
-    return this.classDocs.members?.instance; 
+
+  get apiDocs() {
+    return this.classDocs.members?.instance;
   }
 }
